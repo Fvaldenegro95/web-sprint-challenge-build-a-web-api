795c95a8b18bf0688988dbdc893501b4f23f5742
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-12-03 14:31:27 -0500

    moved all work from the wrong branch over to this one, please remember to fork next time!!

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,34 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateActionId(req, res, next){
+    try{
+        const action = await Action.get(req.params.id)
+        if(!action){
+            res.status(404).json({message: 'No actions with that ID!'})
+        } else{
+            req.action = action
+            next()
+        }
+    }catch(err){
+        next({status: 400, message: 'actual action not found!'})
+    }
+}
+
+async function validateAction (req, res, next){
+    const {project_id, description, notes, completed} = req.body
+    if(req.body.project_id === undefined){
+        next({status: 400, message: 'Requires a Project ID!'})
+    }
+    if(!notes || !notes.trim){
+        next({status: 400, message: 'Requires project notes!'})
+    } else{
+        req.project_id = project_id
+        req.description = description.trim()
+        req.notes = notes.trim()
+        req.completed = completed
+        next()
+    }
+}
+
+module.exports = {validateActionId, validateAction}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,54 @@
 // Write your "actions" router here!
+const Actions = require("./actions-model")
+const express = require("express")
+const router = express.Router()
+const {validateActionId, validateAction} = require("./actions-middlware")
+
+router.get('/', async (req, res ,next) => {
+    try{
+        const actions = await Actions.get()
+        res.status(200).json(actions)
+    } catch(err){
+        next({status: 400, message: 'error Getting!'})
+    }
+})
+
+router.get("/:id", validateActionId, async (req, res, next) => {
+    try{
+        res.status(200).json(req.action)
+    } catch(err){
+        next({status: 400, message: 'error Getting by ID!'})
+    }
+})
+
+router.post("/", async (req, res ,next) => {
+    try{
+        const newAction = await Actions.insert(req.body)
+        console.log(newAction)
+        res.status(201).json(newAction)
+    } catch(err){
+        next({status: 400, message: 'Error posting!'})
+    }
+})
+
+router.put("/:id", validateActionId, validateAction, async (req, res,) => {
+    console.log(req.params.id)
+    const updatedAction = await Actions.update(req.params.id, {
+        project_id: req.project_id,
+        description: req.description,
+        notes: req.notes,
+        completed: req.completed
+        })
+    res.status(200).json(updatedAction)
+})
+
+router.delete("/:id", validateActionId, async (req, res, next) => {
+    try{
+        await Actions.remove(req.params.id)
+        res.json(res.Action)
+    } catch(err){
+        next({status: 400, message: 'Delete cannot be completed!'})
+    }
+})
+
+module.exports = router
